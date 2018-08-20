aef3464dcfba4182cbbc11b3b37d20cad87cdc80
Hao Xiong <hao.xiong@HaodeMacBook-Pro.local>
2018-08-20 11:28:34 +0800

    update 404 error

--- http_server.py
+++ http_server.py
@@ -5,5 +5,8 @@ from src.mod import mod
 app = Flask(__name__)
 app.register_blueprint(mod)
 flask_restful.abort = resp.new_abort
+@app.errorhandler(404)
+def error_404(error):
+    return resp.response(code = resp.SYSTEM_ERR)
 if __name__ == "__main__":
     app.run(host='127.0.0.1',port = 5000,debug=True)
